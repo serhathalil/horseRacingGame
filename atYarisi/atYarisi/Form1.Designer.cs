aba1260fe5ed71533c7652305411f17f163e838d
Serhat Silsüpür <serhatsilsupur@gmail.com>
2017-11-22 13:24:22 +0300

    at yarışı v1

---
+++ atYarisi/atYarisi/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace atYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnYeniden = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(927, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(927, 23);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(926, 23);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(926, 23);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(912, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 362);
+            this.label5.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::atYarisi.Properties.Resources.at3;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 261);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(103, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::atYarisi.Properties.Resources.at2;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 143);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::atYarisi.Properties.Resources.at1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(146, 379);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(620, 84);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Yarış Bilgisi";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSure
+            // 
+            this.lblSure.BackColor = System.Drawing.SystemColors.ControlText;
+            this.lblSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSure.ForeColor = System.Drawing.Color.White;
+            this.lblSure.Location = new System.Drawing.Point(796, 379);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(108, 84);
+            this.lblSure.TabIndex = 10;
+            this.lblSure.Text = "0";
+            this.lblSure.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // btnYeniden
+            // 
+            this.btnYeniden.BackColor = System.Drawing.Color.White;
+            this.btnYeniden.ForeColor = System.Drawing.Color.Black;
+            this.btnYeniden.Location = new System.Drawing.Point(12, 424);
+            this.btnYeniden.Name = "btnYeniden";
+            this.btnYeniden.Size = new System.Drawing.Size(103, 39);
+            this.btnYeniden.TabIndex = 11;
+            this.btnYeniden.Text = "Yeniden Başla";
+            this.btnYeniden.UseVisualStyleBackColor = false;
+            this.btnYeniden.Click += new System.EventHandler(this.btnYeniden_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1000, 489);
+            this.Controls.Add(this.btnYeniden);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btnYeniden;
+    }
+}
+
---
+++ atYarisi/atYarisi/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int birinciatsol, ikinciatsol, ucuncuatsol;
+        int sure;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            birinciatsol = pictureBox1.Left;
+            ikinciatsol = pictureBox2.Left;
+            ucuncuatsol = pictureBox3.Left;
+            btnYeniden.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+           
+            int birinciatgenislik = pictureBox1.Width;
+            int ikinciatgenislik = pictureBox2.Width;
+            int ucuncuatgenislik = pictureBox3.Width;
+            int bitisuzakligi = label5.Left;
+
+            pictureBox1.Left = pictureBox1.Left + rnd.Next(5, 15);
+            pictureBox2.Left = pictureBox2.Left + rnd.Next(5, 15);
+            pictureBox3.Left = pictureBox3.Left + rnd.Next(5, 15);
+
+            if (pictureBox1.Left > pictureBox2.Left + 5 && pictureBox1.Left > pictureBox3.Left + 5)
+            {
+                label6.Text = "1 numaralı at yarışı önde götürüyor";
+            }
+            if (pictureBox2.Left > pictureBox1.Left + 5 && pictureBox2.Left > pictureBox3.Left + 5)
+            {
+                label6.Text = "2 numaralı at iyi bir atakla öne geçti";
+            }
+            if (pictureBox3.Left > pictureBox1.Left + 5 && pictureBox3.Left > pictureBox2.Left + 5)
+            {
+                label6.Text = "3 numaralı at liderliği ele geçirdi";
+            }
+
+            if (birinciatgenislik+pictureBox1.Left>=bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label6.Text = "1 numaralı at kazandı";
+                btnYeniden.Enabled = true;
+            }
+            if (ikinciatgenislik+pictureBox2.Left>=bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label6.Text = "2 numaralı at kazandı";
+                btnYeniden.Enabled = true;
+            }
+            if (ucuncuatgenislik+pictureBox3.Left>=bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label6.Text = "3 numaralı at kazandı";
+                btnYeniden.Enabled = true;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            sure = Convert.ToInt32(lblSure.Text);
+            sure++;
+            lblSure.Text = sure.ToString();
+        }
+
+        private void btnYeniden_Click(object sender, EventArgs e)
+        {
+            btnYeniden.Enabled = false;
+            pictureBox1.Left = 0;
+            pictureBox2.Left = 0;
+            pictureBox3.Left = 0;
+            timer2.Enabled = true;
+            timer1.Enabled = true;
+            sure = 0;
+            sure++;
+            lblSure.Text = sure.ToString();
+            
+        }
+
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+           
+        }
+    }
+}
